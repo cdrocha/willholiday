0cd2ceb2fc7cd3e99e5a7bd7ea50285fc9743023
cdrocha <cdrocha82@gmail.com>
2015-02-26 15:57:52 -0300

    actualice la key de FacebookLogin

--- WillHoliday/Js/login.js
+++ WillHoliday/Js/login.js
@@ -36,7 +36,7 @@ function statusChangeCallback(response) {
 
 window.fbAsyncInit = function() {
     FB.init({
-        appId: '1066772186673164',
+    appId: '1068139036536479',
         cookie: true,  // enable cookies to allow the server to access
         // the session
         xfbml: true,  // parse social plugins on this page
